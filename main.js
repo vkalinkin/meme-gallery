3a9d904719765bebf8d88b2cf2619fc803d215e4
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-01 14:29:22 -0700

    Meme Gallery Final (all functionality added)

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 //console.log('hello, world!');
-var imageUrls = [];
+var imageUrls = ['https://th.bing.com/th/id/OIP.NT2RwbvIz6VKelx98bXIQAHaGv?pid=ImgDet&rs=1',
+  'https://ih0.redbubble.net/image.133003823.2275/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+'https://static1.cbrimages.com/wordpress/wp-content/uploads/2017/04/superhero-meme-display.jpg'];
+
+
+var gallery = document.querySelector('main');
+
 var imageUrlInput = document.querySelector('input');
 var addImageButton = document.querySelector('button');
 
@@ -12,4 +18,17 @@ function performOnClick(){
 
   }
   imageUrlInput.value = "";
+  updateGallery();
+}
+
+function updateGallery(){
+  gallery.innerHTML = '';
+  for (var a = 0; a < imageUrls.length; a++){
+    //console.log("imageUrls array contents: ", imageUrls[a]);
+    var imageElement = document.createElement('img');
+    imageElement.className = 'gallery-image';
+    imageElement.src = imageUrls[a];
+    //console.log(imageElement);
+    gallery.appendChild(imageElement);
+  }
 }
